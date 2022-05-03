dc26f2b04afab160358f9e7d3687493352a8d67c
Digambar Patil <59257416+webdevelopersbc@users.noreply.github.com>
2022-05-03 10:45:38 +0530

    Update class-aud-txt-scripts.php

--- includes/class-aud-txt-scripts.php
+++ includes/class-aud-txt-scripts.php
@@ -53,7 +53,7 @@ class Aud_txt_Scripts {
    		wp_enqueue_style('aud-bootstrap',  'https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css', array(), '', false);
 
 
-   		//wp_enqueue_script('aud-slim', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', array(), time(), true);
+   		wp_enqueue_script('aud-slim', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', array(), time(), true);
 
    		wp_enqueue_script('aud-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js', array(), time(), true);
 
